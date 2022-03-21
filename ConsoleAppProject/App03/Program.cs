6a1cdbe76c38c13f5087b2a8330d09b4bc075514
James Payne <79662097+JamesPayne24@users.noreply.github.com>
2022-03-21 09:49:32 +0000

    1

---
+++ ConsoleAppProject/App03/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Text;
+
+
+
+namespace marks
+
+{
+
+
+
+    class StudentGrade
+
+    {
+
+        static bool ReadInteger(out int n)
+
+        {
+
+            string input = System.Console.ReadLine();
+
+            n = 0;
+
+            try
+
+            {
+
+                n = Convert.ToInt32(input);
+
+                return true;
+
+            }
+
+            catch (System.Exception ex)
+
+            {
+
+                System.Console.WriteLine("Error in the input format\n\n");
+
+                return false;
+
+            }
+
+        }
+
+
+
+        static void Main(string[] args)
+
+        {
+
+            System.Console.WriteLine("Program for simple student grading logic.");
+
+
+
+            const int MAX_STUDENTS = 10;
+
+
+
+            int[] arrMark = new int[MAX_STUDENTS];
+
+            string grade = "";
+
+
+
+            for (int i = 0; i < MAX_STUDENTS; i++)
+
+            {
+
+                System.Console.Write("Enter {0} Student Mark: ", i + 1);
+
+                ReadInteger(out arrMark[i]);
+
+            }
+
+            System.Console.Write("\n\nNo\tMark\tGrade\n");
+
+
+
+            for (int i = 0; i < MAX_STUDENTS; i++)
+
+            {
+
+                if (arrMark[i] > 100)
+
+                    grade = "Error";
+
+                else if (arrMark[i] > 90)
+
+                    grade = "A+";
+
+                else if (arrMark[i] > 70)
+
+                    grade = "B+";
+
+                else if (arrMark[i] > 50)
+                    grade = "C+";
+
+                else if (arrMark[i] > 30)
+
+                    grade = "C";
+
+                else
+
+                    grade = "F";
+
+                System.Console.Write("{0}\t{1}\t{2}\n", i + 1, arrMark[i], grade);
+
+            }
+
+
+
+        }
+
+    }
+
+
+
+}
